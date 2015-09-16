e6b7dc55ef173f6a03d5583838ce38a067c1ece4
Wei Wang <ww85@uw.edu>
2015-09-15 20:15:42 -0700

    handle query for recommendations by userid

--- src/main/java/org/mufasamedia/recommendercore/Engine.java
+++ src/main/java/org/mufasamedia/recommendercore/Engine.java
@@ -1,6 +1,7 @@
 package org.mufasamedia.recommendercore;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
@@ -14,42 +15,60 @@ import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class Engine {
-	
- 	public static String getRecommendation() {
- 		return "test";
- 	}
- 	
- 	public static String testFunc(){
- 		
- 		String returnStr ="";
-		
- 		try {
- 			
- 			DataModel model =
- 					new FileDataModel (new File("C:\\intro.csv"));
- 			UserSimilarity similarity =
- 					new PearsonCorrelationSimilarity (model);
- 			UserNeighborhood neighborhood = 
- 					new NearestNUserNeighborhood (2, similarity, model);
- 			
- 			Recommender recommender = new GenericUserBasedRecommender (
- 					model, neighborhood, similarity);
- 			
- 			List<RecommendedItem> recommendations =
- 					recommender.recommend(1, 1);
- 			
- 			
- 			for (RecommendedItem recommendation : recommendations) {
- 				returnStr += recommendation;
- 			}
- 			
- 			
- 		} catch (Exception e) {
- 			e.printStackTrace();
- 		}
-		
- 		return returnStr;
-		
+
+	public static String getRecommendation() {
+		return "test";
+	}
+
+	public static List<String> getRecommendationList(int userId) {
+		ArrayList<String> result = new ArrayList<String>();
+		try {
+
+			DataModel model = new FileDataModel(
+					new File("/Users/dw/Documents/workspace/mufasamedia-recommendercore/intro.csv"));
+			UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+			UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
+
+			Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+			List<RecommendedItem> recommendations = recommender.recommend(userId, 3);
+
+			for (RecommendedItem recommendation : recommendations) {
+				result.add(recommendation.toString());
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	public static String testFunc() {
+
+		String returnStr = "";
+
+		try {
+
+			DataModel model = new FileDataModel(
+					new File("/Users/dw/Documents/workspace/mufasamedia-recommendercore/intro.csv"));
+			UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+			UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
+
+			Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+			List<RecommendedItem> recommendations = recommender.recommend(1, 1);
+
+			for (RecommendedItem recommendation : recommendations) {
+				returnStr += recommendation;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return returnStr;
+
 	}
 
 }
